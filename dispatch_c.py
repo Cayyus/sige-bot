0767cec99141b65aa1f5efbb4a8199ccd3f86f5d
Cayyus <117181766+Daviehoff@users.noreply.github.com>
2023-06-20 18:22:40 +0600

    Uploading functions for the bot

---
+++ dispatch_c.py
@@ -0,0 +1,65 @@
+import aiohttp
+import xml.etree.ElementTree as ET
+import discord
+from discord import app_commands
+from credentials import headers
+
+async def dispatch(interaction, dispatch_link: str):
+    dispatch_split = dispatch_link.split('id=')
+    dispatch_id = dispatch_split[1]
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f'https://www.nationstates.net/cgi-bin/api.cgi?q=dispatch;dispatchid={dispatch_id}', headers=headers) as response:
+            data = await response.text()
+            root = ET.fromstring(data)
+
+            dispatch = root.find("DISPATCH")
+            if dispatch is None:
+                await interaction.response.send_message("Unable to retrieve resolution information.")
+                return
+                
+            title_ele = dispatch.find("TITLE")
+            if title_ele is None:
+                await interaction.response.send_message("Unable to retrieve resolution information.")
+                return
+                
+            title = title_ele.text
+
+            author_ele = dispatch.find("AUTHOR")
+            if author_ele is None:
+                await interaction.response.send_message("Unable to retrieve resolution information.")
+
+            author = author_ele.text
+            author_link = f"https://www.nationstates.net/nation={author}"
+
+            creation_date_ele = dispatch.find("CREATED")
+            if creation_date_ele is None:
+                await interaction.response.send_message("Unable to retrieve resolution information.")
+                
+            creation_date = creation_date_ele.text
+
+            views_ele = dispatch.find("VIEWS")
+            if views_ele is None:
+                await interaction.response.send_message("Unable to retrieve resolution information.")
+                
+            views = views_ele.text
+
+            score_ele = dispatch.find("SCORE")
+            if score_ele is None:
+                await interaction.response.send_message("Unable to retrieve resolution information.")
+                
+            scores = score_ele.text
+
+            link = f"https://www.nationstates.net/page=dispatch/id={dispatch_id}"
+
+        dispatch_bullet = [
+            f"- `Author`: [{author}]({author_link})",
+            f"- `Date Created`: <t:{creation_date}:D>",
+            f"- `Views`: {views}",
+            f"- `Score`: {scores}"
+        ]
+
+        dispatch_bullet_embed = "\n".join(dispatch_bullet)
+        embed = discord.Embed(title=title, url=link)
+        embed.add_field(name='General Information', value=dispatch_bullet_embed, inline=False)
+        await interaction.response.send_message(embed=embed)
---
+++ nne_c.py
@@ -0,0 +1,60 @@
+import discord
+from discord import app_commands
+import aiohttp
+import re
+from credentials import headers
+
+async def nations_not_endorsing(interaction, nation_name: str, region_name: str):
+    url_endo = f"https://www.nationstates.net/cgi-bin/api.cgi?nation={nation_name}&q=endorsements"
+    url_region_wa = f"https://www.nationstates.net/cgi-bin/api.cgi?region={region_name}&q=wanations"
+    
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url_endo, headers=headers) as response_endo:
+            endo_data = await response_endo.text()
+        
+        async with session.get(url_region_wa, headers=headers) as response_endo_wa:
+            region_wa = await response_endo_wa.text()
+
+    endorsements = re.search(r"<ENDORSEMENTS>(.*?)</ENDORSEMENTS>", endo_data, re.DOTALL)
+    if endorsements:
+        extracted_data = endorsements.group(1)
+        endorsements_list = extracted_data.split(',')
+    else:
+        await interaction.response.send_message("Endorsements not found in the XML data.")
+        return
+    
+    unnations = re.search(r"<UNNATIONS>(.*?)</UNNATIONS>", region_wa, re.DOTALL)
+    if unnations:
+        extracted_data_region = unnations.group(1)
+        unnations_list = extracted_data_region.split(',')
+    else:
+        await interaction.response.send_message("UNNATIONS not found in the XML data.")
+        return
+    
+    unnations_only = list(set(unnations_list) - set(endorsements_list))
+    
+    nations_per_string = 8
+    unnations_strings = [', '.join(unnations_only[i:i+nations_per_string]) for i in range(0, len(unnations_only), nations_per_string)]
+    
+    response_message = f"Nations Not Endorsing {nation_name}:\n"
+    for i, string in enumerate(unnations_strings, 1):
+        response_message += f"String {i}: {string}\n"
+    
+    # Split the response into multiple messages based on character limit
+    messages = []
+    current_message = ""
+    for line in response_message.splitlines():
+        if len(current_message) + len(line) > 2000:
+            messages.append(current_message)
+            current_message = ""
+        current_message += line + "\n"
+    messages.append(current_message)
+    
+    # Send the messages
+    for i, message in enumerate(messages):
+        if i == 0:
+            await interaction.response.send_message(message)
+        else:
+            await interaction.followup.send(message)
+
+
