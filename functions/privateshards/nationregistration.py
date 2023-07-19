11c133b6c2f321a0ce7767944ee93670d9a8a124
Caius <117181766+Cayyus@users.noreply.github.com>
2023-07-19 11:00:36 +0600

    Delete functions/privateshards directory

--- functions/privateshards/nationregistration.py
+++
@@ -1,8 +0,0 @@
-import discord
-from discord import app_commands
-
-async def nation_register(interaction, nation_name: str, nation_password: str):
-    with open('dispatch_nation.txt', 'w') as file:
-        file.write(f'Nation Name_{nation_name} - {nation_name}\n' + f"Nation Password_{nation_name} - {nation_password}")
-        await interaction.response.send_message("Nation Registered!")
-    
