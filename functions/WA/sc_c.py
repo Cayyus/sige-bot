c2d9f27d5504aa180b0297b89f1ab930ff5f2620
Caius <117181766+Cayyus@users.noreply.github.com>
2023-07-24 23:20:03 +0600

    Update sc_c.py

--- functions/WA/sc_c.py
+++ functions/WA/sc_c.py
@@ -6,22 +6,22 @@ from credentials import headers
 
 
 async def sc(interaction, proposal_id: int):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(f'https://www.nationstates.net/cgi-bin/api.cgi?wa=2&id={proposal_id}&q=resolution', headers=headers) as response:
-            data = await response.text()
-            root = ET.fromstring(data)
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f'https://www.nationstates.net/cgi-bin/api.cgi?wa=2&id={proposal_id}&q=resolution', headers=headers) as response:
+                data = await response.text()
+                root = ET.fromstring(data)
 
-            res_element = root.find("RESOLUTION")
-            name = res_element.find("NAME").text
-            author = res_element.find("PROPOSED_BY").text
-            implemented = res_element.find("IMPLEMENTED").text
-            forvotes = res_element.find('TOTAL_VOTES_FOR').text
-            againstvotes = res_element.find("TOTAL_VOTES_AGAINST").text
-            link = f"https://www.nationstates.net/page=WA_past_resolution/id={proposal_id}/council=2"
-            author_nation_link = f"https://www.nationstates.net/nation={author}"
-            _id = res_element.find('COUNCILID').text
-
-
+                res_element = root.find("RESOLUTION")
+                name = res_element.find("NAME").text
+                author = res_element.find("PROPOSED_BY").text
+                implemented = res_element.find("IMPLEMENTED").text
+                forvotes = res_element.find('TOTAL_VOTES_FOR').text
+                againstvotes = res_element.find("TOTAL_VOTES_AGAINST").text
+                link = f"https://www.nationstates.net/page=WA_past_resolution/id={proposal_id}/council=2"
+                author_nation_link = f"https://www.nationstates.net/nation={author}"
+                _id = res_element.find('COUNCILID').text
+            
             bullets = [
                     f"- Author: [{author}]({author_nation_link})",
                     f"- Date of Passing: <t:{implemented}:R>",
@@ -31,4 +31,7 @@ async def sc(interaction, proposal_id: int):
         bullet_embed = '\n'.join(bullets)
         embed = discord.Embed(title=name, url=link)
         embed.add_field(name=f'Information about SC#{_id}', value=bullet_embed, inline=False)
+        embed.set_thumbnail(url="https://www.nationstates.net/images/sc.jpg")
         await interaction.response.send_message(embed=embed)
+    except:
+        await interaction.response.send_message('Resolution not found, try again.')
